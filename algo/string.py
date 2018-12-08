94d4b831c3d2d1daa23a5fe2d9ad1953cce2047e
Le Dai <daile96@qq.com>
2018-12-08 23:46:15 +0800

    Add expression evaluation.

--- algo/string.py
+++ algo/string.py
@@ -1,3 +1,4 @@
+"""String algorithms."""
 from .stack import Stack
 
 
@@ -6,10 +7,53 @@ def checkBrackets(s):
     stack = Stack()
     d = {')': '(', ']': '[', '}': '{'}
     for c in s:
-        if c in set(')]}'):
+        if c in ')]}':
             if not len(stack) or stack.top() != d[c]:
                 return False
             stack.pop()
-        elif c in set('([{'):
+        elif c in '([{':
             stack.push(c)
     return len(stack) == 0
+
+
+def _calc(op, a, b):
+    if op == '+':
+        return a + b
+    if op == '-':
+        return a - b
+    if op == '*':
+        return a * b
+    if op == '/':
+        return a / b
+
+
+def evaluate(expr):
+    """Evaluate arithmetic expressions."""
+    expr = '(' + expr + ')'
+    vals = Stack()
+    ops = Stack()
+    d = {'+': 0, '-': 0, '*': 1, '/': 1, '(': 2, ')': -1}
+    for c in expr:
+        if c.isspace():
+            continue
+        if c in d.keys():
+            while len(ops) and d[ops.top()] >= d[c] and ops.top() != '(':
+                assert len(vals) >= 2, 'lacking oprands'
+                op = ops.pop()
+                b = vals.pop()
+                a = vals.pop()
+                vals.push(_calc(op, a, b))
+            if c == ')':
+                assert len(ops), 'more ")" than "("'
+                ops.pop()
+            else:
+                ops.push(c)
+        else:
+            vals.push(int(c))
+    assert len(vals) == 1, 'more than one values left'
+    assert len(ops) == 0, 'operators not empty'
+    return vals.top()
+
+
+if __name__ == '__main__':
+    print(evaluate('3(1+3)'))
--- tests/test_string.py
+++ tests/test_string.py
@@ -1,4 +1,5 @@
-from algo.string import checkBrackets
+import pytest
+from algo.string import checkBrackets, evaluate
 
 
 def test_check_brackets():
@@ -9,3 +10,23 @@ def test_check_brackets():
     assert not checkBrackets(']string')
 
     assert not checkBrackets('s(t[r{)}i]ng')
+
+
+def test_evaluate():
+    expr = '(1-(2+3)*4)/5'
+    assert evaluate(expr) == -3.8
+
+    expr = '1 - 2 + 3'
+    assert evaluate(expr) == 2
+
+    with pytest.raises(AssertionError) as e:
+        evaluate('((1+2)/3')
+        assert e.message == 'operators not empty'
+
+    with pytest.raises(AssertionError) as e:
+        evaluate('1+2*(')
+        assert e.message == 'more than one values left'
+
+    with pytest.raises(AssertionError) as e:
+        evaluate(')')
+        assert e.message == 'more ")" than "("'
