be029e115c15d4926180fb2d20ab29975c1f6abf
Le Dai <daile96@qq.com>
2018-12-20 23:50:34 +0800

    hmmm

--- algo/sorting.py
+++ algo/sorting.py
@@ -8,3 +8,10 @@ def heapSort(array, reverse=False):
         heap = Heap(array, lambda a, b: a > b)
     for _ in range(len(array)):
         heap.pop()
+
+
+def quickSort(array):
+    if len(array) <= 1:
+        return
+    m = array[0]
+    pass
